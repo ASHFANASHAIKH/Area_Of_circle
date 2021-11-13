36d956fd9673b8cf079192ae9138b4b5ea825431
ashfana <ashfana@DESKTOP-NPR6MTA>
2021-11-13 12:03:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_Of_circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r;
+            double A;
+            Console.WriteLine("Enter the radius:");
+            r = Convert.ToInt32(Console.ReadLine());
+            A = (3.142) * r * r;
+            Console.WriteLine("The Area of circle of Given radius is=" + A);
+            Console.ReadLine();
+
+        }
+    }
+}
